016275001c631ebe8d08813dd986dd77e2bec06a
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-04 15:57:54 +0100

    Improve UX on pwd validation

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+check_pwd();
+
 function check_pwd() {
     const pwd = document.getElementById("password").value;
     const confirm_pwd = document.getElementById("confirm_password").value;
 
     if (pwd === "" || confirm_pwd === "" || pwd !== confirm_pwd) {
         document.getElementById("pwd_error_msg").innerText = "* Passwords do not match";
+        document.getElementById("password").style.border = "2px solid red";
+        document.getElementById("confirm_password").style.border = "2px solid red";
     }
     else {
         document.getElementById("pwd_error_msg").innerText = "";
+        document.getElementById("password").style.border = "";
+        document.getElementById("confirm_password").style.border = "";
     }
 }
